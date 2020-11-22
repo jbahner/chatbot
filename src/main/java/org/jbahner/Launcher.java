abd0be32d3b3a04aeb1dee3ec0ce35f99af7cdc6
Julian Bahner <julian@bahner.de>
2020-11-22 15:36:43 +0100

    Initial commit with chatbot example

---
+++ src/main/java/org/jbahner/Launcher.java
@@ -0,0 +1,9 @@
+package org.jbahner;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Bot bot = new Bot();
+        bot.registerFeatures();
+        bot.start();
+    }
+}
